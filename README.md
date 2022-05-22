e63b123bdd901cefe79b147783a3071953218fde
Arch0zz <104281088+scriptskali@users.noreply.github.com>
2022-05-21 21:17:27 -0400

    Update and rename README.md to WHATSAPP-HACK.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# WHATSAPP-HACK
---
+++ WHATSAPP-HACK.py
@@ -0,0 +1,19 @@
+# WHATSAPP-HACK
+#
+#created by se4ars3
+#
+import time
+import os
+os.system("clear")
+print("_/﹋\_")
+print("(҂`_´)")
+print("<,︻╦╤─ ҉ - -")
+print("_/﹋\_[Whatsapp Hack]")
+
+option = int(input("[1]BOMBER MESSAGE\n[2]BOMBER CALL\noption@root:~#"))
+    
+number = input("number>")
+i = 0
+while i<20:
+  print(f"[+]BOMBER WHATSAPP DISPARANDO>>>{number}")
+   
